75735ea01e643db0b816c2ceb7f74369259d4a11
Mitesh <miteshsonagra5576@gmail.com>
2025-04-06 16:46:44 +0530

    change cors

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 // Configure CORS
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    // origin: 'https://insta-download-six.vercel.app/' || 'http://localhost:3000',
+    origin: '*',
     credentials: true
 }));
 
